e33324cabd456bd6d7ec5e1cbdcce149b162b7e4
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-22 17:44:28 +0100

    Add SQL solution

---
+++ Associate-Data-Analyst-in-SQL/Data-Manipulation-in-SQL/Window-Functions/Putting-the-CTEs-together.sql
@@ -0,0 +1,10 @@
+-- Exercise
+-- Putting the CTEs together
+
+-- Instructions
+-- Declare the home and away CTEs before your main query.
+-- Join your CTEs to the match table using a LEFT JOIN.
+-- Select the relevant data from the CTEs into the main query.
+-- Select the date from match, team names from the CTEs, and home/away goals from match in the main query.
+
+-- MY SOLUTION:
